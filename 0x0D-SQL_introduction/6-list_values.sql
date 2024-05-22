0707ec6f26d7943f5c09c4bb689ccd824e3b92b1
Amna2011 <amna2011@gmail.com>
2024-05-21 19:44:29 -0700

    my 2 commit

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- 3 my a script that lists all rows of the table first_table from the database
+-- hbtn_0c_0 in your MySQL server.
+SELECT * FROM first_table
+WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
